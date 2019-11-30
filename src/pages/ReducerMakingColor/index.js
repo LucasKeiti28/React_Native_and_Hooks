20907a166656922ba50468908f2371bfee103446
LucasKeiti28 <lucaskeiti28@gmail.com>
2019-11-30 16:19:47 -0300

    useReducer: state, dispatch, actions, reducer, payload.

--- src/pages/ReducerMakingColor/index.js
+++ src/pages/ReducerMakingColor/index.js
@@ -1,12 +1,88 @@
-import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import React, { useReducer } from "react";
+import { View, Text } from "react-native";
 
-// import { Container } from './styles';
+import ColorCount from "../../components/ColorCount";
 
-export default function ReducerMakingColor() {
+const COLOR_INCREMENT = 15;
+
+const reducer = (state, action) => {
+  // state === {red: number, green: number, blue: number}
+  // action === {type: 'change_red' | 'change_green' | 'change_blue', payload: 15 || -15}
+
+  switch (action.type) {
+    case "change_red":
+      if (state.red + action.payload > 255 || state.red + action.payload < 0) {
+        return state;
+      }
+      return { ...state, red: state.red + action.payload };
+    case "change_green":
+      if (
+        state.green + action.payload > 255 ||
+        state.green + action.payload < 0
+      ) {
+        return state;
+      }
+      return { ...state, green: state.green + action.payload };
+    case "change_blue":
+      if (
+        state.blue + action.payload > 255 ||
+        state.blue + action.payload < 0
+      ) {
+        return state;
+      }
+      return { ...state, blue: state.blue + action.payload };
+    default:
+      return state;
+  }
+};
+
+const NewMakingColor = () => {
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+  const { red, green, blue } = state;
+
   return (
-    <SafeAreaView>
-      <Text>Usando Reducer.</Text>
-    </SafeAreaView>
+    <View>
+      <ColorCount
+        color="Red"
+        onIncrease={() =>
+          dispatch({ type: "change_red", payload: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ type: "change_red", payload: -1 * COLOR_INCREMENT })
+        }
+      />
+      <ColorCount
+        color="Green"
+        onIncrease={() =>
+          dispatch({ type: "change_green", payload: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ type: "change_green", payload: -1 * COLOR_INCREMENT })
+        }
+      />
+      <ColorCount
+        color="Blue"
+        onIncrease={() =>
+          dispatch({ type: "change_blue", payload: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ type: "change_blue", payload: -1 * COLOR_INCREMENT })
+        }
+      />
+
+      <Text>COLOR: RGB({`${red}, ${green}, ${blue}`})</Text>
+
+      <View
+        style={{
+          width: 300,
+          height: 300,
+          borderRadius: 150,
+          margin: 35,
+          backgroundColor: `rgb(${red},${green},${blue})`
+        }}
+      />
+    </View>
   );
-}
+};
+
+export default NewMakingColor;
